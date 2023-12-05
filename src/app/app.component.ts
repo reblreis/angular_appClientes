433ff5de5808766a6179649c64b696cc139aa204
reblreis <rebreis88@gmail.com>
2023-12-05 18:34:08 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/layout/navbar/navbar.component'; 
+import { LoginComponent } from './components/pages/login/login.component'; 
+import { RegisterComponent } from './components/pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    NavbarComponent, 
+    LoginComponent, 
+    RegisterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component'; 
+import { RegisterComponent } from './components/pages/register/register.component';
 
-export const routes: Routes = [];
+/* 
+    Mapeamento das rotas do projeto para 
+    exibir / renderizar componentes 
+*/
+export const routes: Routes = [
+    { 
+        path: 'app/login', 
+        component: LoginComponent 
+    }, 
+    { 
+        path: 'app/register', 
+        component: RegisterComponent 
+    }
+    { 
+        path: '', /* raiz do projeto */ 
+        pathMatch: 'full', 
+        redirectTo: '/app/login' 
+    }
+];
